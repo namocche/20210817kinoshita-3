1ba868d7903e4c1e8e694fb1fd32d5a155db79d1
なもちゃん <user@usernoMacBook-Air.local>
2021-08-17 09:38:08 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/ドロワーメニュー/ 
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
+});
